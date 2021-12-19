9050b85c53a3250aa74d316f67bc6b06defa205b
kazuya sasakura <dropout.college2014@gmail.com>
2021-12-19 18:06:14 +0900

    Initial Commit

---
+++ hashTagTestUITests/hashTagTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  hashTagTestUITestsLaunchTests.swift
+//  hashTagTestUITests
+//
+//  Created by 笹倉一也 on 2021/12/19.
+//
+
+import XCTest
+
+class hashTagTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
